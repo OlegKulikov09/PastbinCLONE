36fdd67ccd9a2b62c7118c9ea6fcd1581c94df13
Oleg Kulikov <kulikovoleg@list.ru>
2024-08-20 18:11:48 +0200

    with login exception

--- src/main/java/com/OlegKulikov/pastbinclone/try_1/Repositories/UserRepository.java
+++ src/main/java/com/OlegKulikov/pastbinclone/try_1/Repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.OlegKulikov.pastbinclone.try_1.Repositories;
 
 import com.OlegKulikov.pastbinclone.try_1.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
         User findById(int id);
+        Optional<User> findByLogin(String login);
     }

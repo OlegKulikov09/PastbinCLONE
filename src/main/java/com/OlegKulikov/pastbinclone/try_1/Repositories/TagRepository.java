9b2982fc331b3e10992c1a3fca2ace4d09492e89
Oleg Kulikov <kulikovoleg@list.ru>
2024-08-29 15:36:03 +0200

    after text viewing

--- src/main/java/com/OlegKulikov/pastbinclone/try_1/Repositories/TagRepository.java
+++
@@ -1,8 +0,0 @@
-package com.OlegKulikov.pastbinclone.try_1.Repositories;
-
-import com.OlegKulikov.pastbinclone.try_1.model.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Integer> {
-    Tag findByNameOfTag(String nameOfTag);
-}
